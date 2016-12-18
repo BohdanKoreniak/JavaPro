213dee0709f22b4145cc7161fdacf2e50c5d08ff
Bohdan Koreniak <imrahill120@gmail.com>
2016-12-18 14:00:22 +0200

    Initial commit

---
+++ JavaPro/src/pro/lesson2/Main.java
@@ -0,0 +1,14 @@
+package pro.lesson2;
+
+import com.bohdan.java.pro.lesson2.managers.PersonManager;
+
+/**
+ * Created by Bohdan on 17.12.2016.
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        new PersonManager().run();
+    }
+}
